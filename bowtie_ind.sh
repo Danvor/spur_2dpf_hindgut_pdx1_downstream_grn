d01179e0dcbfabe8af5420cd8381bc89d92cfff7
Danvor <30661403+Danvor@users.noreply.github.com>
2023-05-10 18:07:47 +0200

    Add files via upload

---
+++ bowtie_ind.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --job-name=bowtie2_index
+#SBATCH --cpus-per-task=16
+#SBATCH --output=log_bowtie2.txt
+module load bowtie/2.3.4.1
+mkdir $2_bowtie_ind
+srun bowtie2-build --threads 32 $1 $2_bowtie_ind/$2

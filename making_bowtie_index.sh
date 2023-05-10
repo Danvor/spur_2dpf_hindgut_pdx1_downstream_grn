92a1c1e7c23a5c2ac83400a20e7922b9d9466e2b
Danvor <30661403+Danvor@users.noreply.github.com>
2023-05-10 18:14:04 +0200

    Update making_bowtie_index.sh

--- making_bowtie_index.sh
+++ making_bowtie_index.sh
@@ -4,5 +4,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --output=log_bowtie2.txt
 module load bowtie/2.3.4.1
+#$1 is genome fasta
+#$2 is prefix for genome name
 mkdir $2_bowtie_ind
 srun bowtie2-build --threads 32 $1 $2_bowtie_ind/$2
